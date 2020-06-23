4673b90c673365d8d8e7ead3efc1dd355c99b318
Chetan Singh <chetan.singh.0602@gmail.com>
2020-06-22 21:19:29 -0400

    added note cell file

---
+++ Note_WeThree/Models/NoteCell.swift
@@ -0,0 +1,13 @@
+//
+//  NoteCell.swift
+//  Note_WeThree
+//
+//  Created by Chetan on 2020-06-22.
+//  Copyright © 2020 Chaitanya Sanoriya. All rights reserved.
+//
+
+import UIKit
+
+class NoteCell: UITableViewCell {
+    
+}
